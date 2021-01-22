bee2c11e02d87e1cb6222c61af6afa31da21f46b
sandeep-svg <sandeeparey987@gmail.com>
2021-01-22 23:25:08 +0530

    smtp

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,22 +59,19 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :test
-  host = 'example.com'
-  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
-  #These settings are for the sending out email for active admin and consequently the   devise mailer
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.perform_deliveries = true
-ActionMailer::Base.raise_delivery_errors = true
-ActionMailer::Base.smtp_settings = 
-{
-
-  :address            => 'smtp.gmail.com',
-  :port               => 587,
-  :domain             => 'gmail.com', #you can also use google.com
-  :authentication     => :plain,
-  :user_name          => 'welcomemailfromsampleapp@gmail.com',
-  :password           => 'Dell951!'
-}
+  config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { :host => 'sampleon.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "welcomemailfromsampleapp@gmail.com",
+    password: "Dell951!", 
+    domain: "smtp.gmail.com",
+    openssl_verify_mode: "none",
+  }
 end
