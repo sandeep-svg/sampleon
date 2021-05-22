e84f9514d0345d985ec5d755e051fa635fc8403f
sandeep-svg <sandeeparey987@gmail.com>
2021-05-22 17:23:09 +0530

    update

--- lib/python/sample.py
+++ lib/python/sample.py
@@ -5,5 +5,5 @@ pincode=data[0]
 date=data[1]
 url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode={}&date={}".format(pincode, date)
 header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'}
-result = requests.get( url, headers=header).json()
-print(result['centers'])
+result = requests.get( url, headers=header)
+print(result.text)
