45b4d54c6c3115d2b94b0db301687322f8360ae8
sandeep-svg <sandeeparey987@gmail.com>
2021-05-22 16:12:04 +0530

    updated

--- app/controllers/vaccine_controller.rb
+++ app/controllers/vaccine_controller.rb
@@ -3,6 +3,6 @@ class VaccineController < ApplicationController
     permitted=params.require(:vaccine).permit(:pincode,:date)
     pincode=permitted[:pincode]
     date=permitted[:date]
-    @sample = `python lib/assets/python/sample.py "#{pincode},#{date}"`
+    @sample = `python sample.py "#{pincode},#{date}"`
   end
 end
