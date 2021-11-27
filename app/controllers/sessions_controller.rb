5bf0afa3e88c25517598ae26ecaac7bb9fceea61
sandeep-svg <72125875+sandeep-svg@users.noreply.github.com>
2021-11-27 09:47:20 +0530

    handling wrong users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,14 @@
 class SessionsController < ApplicationController
-  attr_accessor :login_otp, :new_login_user
+  attr_accessor :login_otp, :new_login_user, :user_flag
   $login_otp = nil
+  @@user_flag = nil
   def new
   end
   def login_with_otp
+    if @@user_flag
+      flash[:invalid_user] = 'User not exist'
+      @@user_flag = nil
+    end
   end
   def login_methods
   end
@@ -16,9 +21,15 @@ class SessionsController < ApplicationController
       UserMailer.login_mail(user , ua , city = request.location.city).deliver!
       redirect_to user
     elsif params[:commit] == "Request otp"
-      $login_otp = UsersController.generate_otp
-      UserMailer.send_login_otp(user, $login_otp).deliver!
-      redirect_to verify_otp_path
+      if user.nil?
+        @@user_flag = true
+        redirect_to login_otp_path
+      else
+        @@user_flag = nil
+        $login_otp = UsersController.generate_otp
+        UserMailer.send_login_otp(user, $login_otp).deliver!
+        redirect_to verify_otp_path
+      end
     else
       flash[:danger]='    invalid credentials!!'
       render 'new'
