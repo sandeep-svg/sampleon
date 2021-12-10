974b09aa46fae7e4afd3d3e19df2fad761300fe5
sandeep-svg <72125875+sandeep-svg@users.noreply.github.com>
2021-12-10 23:13:55 +0530

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module SampleApp
 
     # config/application.rb
      config.assets.initialize_on_precompile = false
+     config.time_zone = 'Chennai'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
